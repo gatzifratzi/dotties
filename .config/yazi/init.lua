ff4fb11883fae6af353fa3f94fa39b682fa7c966
jakob <jakob.gatz@gmail.com>
2025-06-03 17:31:06 +0200

    my commit

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,11 @@
+--  Git Plugin  --
 require("git"):setup()
+
+--  full Border Plugin  --
+require("full-border"):setup {
+    -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+    type = ui.Border.ROUNDED,
+}
+
+--  relative motions plugin --
+require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true, enter_mode = "cache_or_first", only_motions = false })
