de86c7faf58eb0352d25290fc35c26b208fac4fe
jakob <jakob.gatz@gmail.com>
2025-06-03 20:56:29 +0200

    der letzte commit hat yazi plugins hinzugefuegt und keybinds fuer diese eingefuehrt.
    dieser commit hat ebenfalls paar plugins eingefuehrt und den .config
    ordner im allgemeinen aufgeraeumt

--- .config/sketchybar/items/calendar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item calendar right \
-        --set calendar icon= \
-                       update_freq=10 \
-                       script="$PLUGIN_DIR/calendar.sh"
--- .config/sketchybar/items/front_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item front_app left \
-        --set front_app \
-            icon.drawing=on \
-            script="$PLUGIN_DIR/front_app.sh" \
-        --subscribe front_app front_app_switched
